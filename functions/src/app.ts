1353bcb35afa2ac67e40d834afa5f18e8079e24b
Jazmin Esparza <jazminesp71@gmail.com>
2026-02-27 00:14:18 -0600

    refactor: ajustes en inicialización de app, configuración firebase y middleware de validación

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -6,6 +6,16 @@ import inventoryMovementRoutes from "./modules/movement/InventoryMovement.routes
 export const app = express();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    system: "Nail Chic Management System",
+    api_status: "Operational",
+    environment: "Production",
+    version: "1.0.0",
+  });
+});
+
 app.use("/categories", categoryRoutes);
 app.use("/inventory-items", inventoryRoutes);
 app.use("/inventory-movements", inventoryMovementRoutes);
--- functions/src/middlewares/validateBody.ts
+++ functions/src/middlewares/validateBody.ts
@@ -1,16 +1,16 @@
-import { Request, Response, NextFunction } from "express";
+import {Request, Response, NextFunction} from "express";
 
 export const validateBody =
   (schema: any) =>
-  (req: Request, res: Response, next: NextFunction): void => {
-    const { error } = schema.validate(req.body);
+    (req: Request, res: Response, next: NextFunction): void => {
+      const {error} = schema.validate(req.body);
 
-    if (error) {
-      res.status(400).json({ error: error.details[0].message });
-      return; // aquí solo termina la función, no retornas el Response
-    }
+      if (error) {
+        res.status(400).json({error: error.details[0].message});
+        return; // aquí solo termina la función, no retornas el Response
+      }
 
-    next();
-  };
+      next();
+    };
 
 
